2f25202743fbea959a1ff4b828a102a575e0fd3d
gabriel-aires09 <aires514@gmail.com>
2022-03-06 23:21:34 -0300

    Atualizando o script para o Void linux e adicionando o dwm

--- void.sh
+++ void.sh
@@ -1,51 +1,66 @@
-#!/bin/bash
+#!/bin/env bash
 
-clear
-echo "Please select:
-1. Install everything
-2. Update and upgrade distro
-3. Install common aps
-4. Install Vundle/vim
-5. Change shell bash to zsh
-6. Move dotfiles to HOME
-0. Do nothing and exit program
-"
+# VARIABLES
+PACKAGES="zsh zsh-autosuggestions zsh-syntax-highlighting neofetch htop bpytop vim git wget curl lsd alacritty obs xdg-user-dirs bspwm unzip xz git opendoas zsh zsh-autosuggestions zsh-syntax-highlighting neofetch papirus-icon-theme nitrogen rofi autorandr neovim lxappearance alsa-utils neovim pavucontrol" 
+DWM="dwm base-devel libX11-devel libXft-devel libXinerama-devel freetype-devel fontconfig-devel"
+VUNDLE="https://github.com/gmarik/Vundle.vim.git"
+
 void_menu () {
-	read -p "Enter Selection [0-6] > "
-	case "$REPLY" in
-		0) echo "Going back..."
-			break
-			;;
-		1) echo "Its now time to install everything"
-			void_linux
-			;;
-		2) echo "Making the necessary updates..."
-			xbps-install xbps -u
-			xbps-install -Suv
-			;;
-		3) echo "Now it is time to install the apps..."
-			programs
-			# sudo xbps-install -S zsh zsh-autosuggestions zsh-syntax-highlighting neofetch htop bpytop vim git wget curl lsd alacritty obs-studio xdg-user-dirs bspwm unzip xz 
-			;;
-		4) echo "Installing Vundle"
-			git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-			;;
-		5) echo "Changin shell to zsh"
-			chsh -s $(which zsh)
-			;;
-		6) echo "Moving dotfiles to Home"
-			cp .vimrc .alacritty.yml .zshrc ~/
-			;;
-		*) echo "Invalid entry" >&2
-			exit 1
-			;;
-	esac
+	while true;do
+		clear
+		echo "Please select:
+		1. Install everything
+		2. Update and upgrade distro
+		3. Install common aps
+		4. Install Vundle/vim
+		5. Change shell bash to zsh
+		6. Move dotfiles to HOME
+		7. Install JetBrainsMono Nerd Fonts
+		8. Install DWM
+		0. Do nothing and exit program
+		"
+		read -p "Enter Selection [0-6] > "
+		case "$REPLY" in
+			0) echo "Going back..."
+				break
+				;;
+			1) echo "Its now time to install everything"
+				void_linux
+				;;
+			2) echo "Making the necessary updates..."
+				#xbps-install xbps -u
+				xbps-install -Suv
+				;;
+			3) echo "Now it is time to install the apps..."
+				$PROGRAMS
+				;;
+			4) echo "Installing Vundle"
+				git clone $VUNDLE
+				cp .vim ~/
+				;;
+			5) echo "Changin shell to zsh"
+				chsh -s $(which zsh)
+				;;
+			6) echo "Moving dotfiles to Home"
+				cp .vimrc .alacritty.yml .zshrc ~/
+				;;
+			7) echo "Install JetBrains Nerd Fonts"
+				cp -r JetBrainsMono /usr/share/fonts
+				fc-cache -fv
+				;;
+			8) echo "Installing DWM and dependencies"
+				xbps-install -S $DWM
+				*) echo "Invalid entry" >&2
+				exit 1
+				;;
+		esac
+	done
 }
 
 void_linux () {
 	
 	# Update and upgrade everything
-	xbps-install xbps -u
+	#xbps-install xbps -u
 	xbps-install -Suv
 
 	# script to check if the programs are already installed into the system
@@ -62,14 +77,29 @@ void_linux () {
 		curl
 		lsd
 		alacritty
-		obs-studio
+		obs
 		xdg-user-dirs
 		bspwm
 		unzip
 		rofi
 		xz
+		git
+		opendoas 
+		zsh 
+		zsh-autosuggestions 
+		zsh-syntax-highlighting 
+		neofetch papirus-icon-theme 
+		nitrogen 
+		rofi 
+		autorandr 
+		neovim 
+		lxappearance 
+		alsa-utils 
+		neovim 
+		pavucontrol
 	)
-
+	
+	#font: https://github.com/Diolinux/Linux-Mint-19.x-PosInstall
 	for name_of_program in ${PROGRAMS_INSTALL[@]}; do
 		if ! xbps-query -l | awk '{ print $2 }' | grep -q $name_of_program; then
 			xbps-install -S "$name_of_program"
@@ -80,7 +110,8 @@ void_linux () {
 	done
 
 	# Vundle
-	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	git clone $VUNDLE
+	cp .vim ~/
 
 	# Changing shell
 	chsh -s $(which zsh)
@@ -88,37 +119,6 @@ void_linux () {
 	# Dotfiles
 	cp .vimrc .alacritty.yml .zshrc ~/
 
-}
-
-programs () {
-
-	PROGRAMS_INSTALL=(
-		zsh
-		zsh-autosuggestions
-		zsh-syntax-highlight
-		neofetch
-		htop
-		bpytop
-		vim
-		git
-		wget
-		curl
-		lsd
-		alacritty
-		xdg-user-dirs
-		bspwm
-		unzip
-		rofi
-		xz
-	)
-
-	for name_of_program in ${PROGRAMS_INSTALL[@]}; do
-		if ! xbps-query -l | awk '{ print $2 }' | grep -q $name_of_program; then
-			xbps-install -S "$name_of_program"
-		else
-			echo "[Already installed]" - $name_of_program
-		fi
-	done
 }
 
 # calling the menu function
